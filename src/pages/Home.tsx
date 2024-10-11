97fe93d7a6296f9f74315382b8ab05d37b5f599f
ConstIX <x.dilshd03@gmail.com>
2024-10-11 20:47:42 +0300

    feat: add pagination

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { Add } from '@mui/icons-material'
-import { Autocomplete, Box, Button, debounce, TextField } from '@mui/material'
+import { Autocomplete, Box, Button, Pagination, TextField } from '@mui/material'
 import { FC, useState } from 'react'
 import TemplateCard from '../components/template/TemplateCard'
 import { useGetTemplatesQuery } from '../redux/services/templates'
@@ -9,11 +9,24 @@ const Home: FC = () => {
   const token = localStorage.getItem('token')
 
   const [searchValue, setSearchValue] = useState<string>('')
-  const onChangeInput = debounce((event: string) => setSearchValue(event), 300)
+  const [currentPage, setCurrentPage] = useState<number>(1)
+  const templatesPerPage = 5
+
+  const handleSearchInputChange = (event: string) => {
+    setSearchValue(event)
+    setCurrentPage(1)
+  }
 
   const filteredTemplates = searchValue
     ? templates?.filter((template) => template.title.toLowerCase().includes(searchValue.toLowerCase()))
     : templates
+
+  const currentTemplates = filteredTemplates?.slice(
+    (currentPage - 1) * templatesPerPage,
+    currentPage * templatesPerPage
+  )
+
+  const totalPages = Math.ceil((filteredTemplates?.length || 0) / templatesPerPage)
 
   return (
     <Box className="mx-auto w-full max-w-7xl px-3 py-32 md3:mt-24">
@@ -24,7 +37,7 @@ const Home: FC = () => {
           disableClearable
           options={templates?.map((obj) => obj.title) || []}
           sx={{ width: 350 }}
-          onInputChange={(_, newInputValue) => onChangeInput(newInputValue)}
+          onInputChange={(_, newInputValue) => handleSearchInputChange(newInputValue)}
           renderInput={(params) => <TextField {...params} type="search" label="Search templates" />}
         />
 
@@ -36,7 +49,18 @@ const Home: FC = () => {
       </Box>
 
       <Box className="space-y-5">
-        {filteredTemplates && filteredTemplates.map((obj) => <TemplateCard key={obj.id} {...obj} />)}
+        {currentTemplates && currentTemplates.map((obj) => <TemplateCard key={obj.id} {...obj} />)}
+      </Box>
+
+      {/* Компонент Pagination */}
+      <Box mt={4} display="flex" justifyContent="center">
+        <Pagination
+          count={totalPages}
+          page={currentPage}
+          onChange={(_, value) => setCurrentPage(value)}
+          color="primary"
+          shape="rounded"
+        />
       </Box>
     </Box>
   )
