6e4fb0c1b790b3b764970a19860c4f36c56ba4bf
ConstIX <x.dilshd03@gmail.com>
2024-11-07 16:09:01 +0300

    fix: new access token

--- src/redux/services/salesforce.ts
+++ src/redux/services/salesforce.ts
@@ -6,7 +6,7 @@ export const salesforceApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://creative-narwhal-hoj9n3-dev-ed.trailblaze.my.salesforce.com//services/data/v62.0/sobjects',
     prepareHeaders: (headers) => {
-      const accessToken = '00Dd2000004EroX!AQEAQI7gbsOGre1lIZmqjrfFG3BM6gIvqNns3w1TM9x8n0KZfghmkz6MK9Z1U2zqI6emLWILN6uPReIbxOB4iGGEj.hkm0Ms'
+      const accessToken = '00Dd2000004EroX!AQEAQM4oWjCIUK_NeIv7WvXUuIopCthabkRqb2SKme1FHDQEMcwEWpLn3cKmeYaZ0XAm8n6WhiUAKEjGh9.JozI6rOZ2UG5E'
       headers.set('Authorization', `Bearer ${accessToken}`)
       return headers
     }
